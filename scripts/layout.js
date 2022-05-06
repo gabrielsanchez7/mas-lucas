ac67f412386be769cf2cf2c493e3d11e6493e531
Gabriel Sánchez <gabrielsanchez@MacBook-Pro-de-Gabriel.local>
2022-05-05 21:13:25 -0500

    submenu

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,33 +1,50 @@
 /** Check if layout has loaded */
 const layoutInterval = setInterval(() => {
-    const header = document.querySelector('header')
-    const footer = document.querySelector('footer')
-    if(header != null && footer != null) {
-        loadScripts()
-        clearInterval(layoutInterval)
-    }
+	const header = document.querySelector('header')
+	const footer = document.querySelector('footer')
+	if (header != null && footer != null) {
+		loadScripts()
+		clearInterval(layoutInterval)
+	}
 }, 10)
 
 const loadScripts = () => {
 
-    loadUserMenu()
-    login()
+	loadUserMenu()
+	login()
+	chooseSubmenu()
+	toggleSubmenu()
 
 }
 
 /** Show user menu options by login status */
 const loadUserMenu = () => {
-    const loginStatus = localStorage.getItem('login')
-    const condition = loginStatus == null ? 'unlogged' : 'logged'
-    const tag = document.querySelector(`if[data-condition="${condition}"]`)
-    const userHtml = tag?.innerHTML.trim()
-    tag.outerHTML = userHtml
+	const loginStatus = localStorage.getItem('login')
+	const condition = loginStatus == null ? 'unlogged' : 'logged'
+	const tag = document.querySelector(`if[data-condition="${condition}"]`)
+	const userHtml = tag?.innerHTML.trim()
+	tag.outerHTML = userHtml
 }
 
-function login() {
-    const loginButton = document.querySelector('.header__user')
-    loginButton.addEventListener('click', _ => {
-        localStorage.setItem('login', 'true')
-        location.reload()
-    })
+/** Click on login button */
+const login = () => {
+	const loginButton = document.querySelector('.header__user-action')
+	loginButton.addEventListener('click', _ => {
+		localStorage.setItem('login', 'true')
+		location.reload()
+	})
+}
+
+/** Click on logout button */
+const chooseSubmenu = () => {
+	const logout = document.querySelector('.header .submenu__item--logout')
+	logout.addEventListener('click', _ => {
+		localStorage.removeItem('login')
+		location.reload()
+	})
+}
+
+/** Show or hide submenu */
+const toggleSubmenu = () => {
+
 }
